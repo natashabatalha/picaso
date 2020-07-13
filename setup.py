4c61930da691cfb935847979d6143a8ae796c658
Natasha Batalha <natasha.e.batalha@gmail.com>
2020-07-12 17:24:05 -0700

    minor release

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ except ImportError:
 # to this sample package.
 setup(
     name='picaso', 
-    version = '2.0',
+    version = '2.0.1',
     description = 'planetary intesity code for atmospheric scattering observations',
     long_description = 'README.rst',
     author = 'Natasha E. Batalha',
