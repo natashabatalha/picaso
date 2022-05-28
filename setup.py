5872a73deda5dc2f90fc2ea000e9878a933af01d
Natasha Batalha <natasha.e.batalha@gmail.com>
2022-05-27 17:27:49 -0700

    add pressure level note

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ except ImportError:
 # to this sample package.
 setup(
     name='picaso', 
-    version = '2.3.1',
+    version = '2.3.2',
     description = 'planetary intesity code for atmospheric scattering observations',
     long_description = 'README.rst',
     author = 'Natasha E. Batalha',
