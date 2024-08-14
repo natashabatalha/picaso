8ba5f9dcae21b8098f5d04ca42147c3979a5aa87
aditya-sengupta <adityars@ucsc.edu>
2024-08-14 09:17:36 -0700

    new start for momentclouds

---
+++ docs/notebooks/climate/12a_BrownDwarf.py
@@ -0,0 +1,64 @@
+# %%
+import os
+import numpy as np
+import time
+
+time.sleep(5)
+
+"""
+import warnings
+warnings.filterwarnings('ignore')
+import picaso.justdoit as jdi
+import picaso.justplotit as jpi
+jpi.output_notebook()
+import astropy.units as u
+import numpy as np
+import matplotlib.pyplot as plt
+
+#1 ck tables from roxana
+mh = '+000'#'+0.0' #log metallicity
+CtoO = '100'#'1.0' # CtoO ratio
+
+ck_db = f'/Users/adityasengupta/projects/clouds/picaso/data/kcoeff_2020/sonora_2020_feh{mh}_co_{CtoO}.data.196'
+#sonora bobcat cloud free structures file
+sonora_profile_db = '/Users/adityasengupta/projects/clouds/picaso/data/sonora_bobcat/structures_m+0.0'
+
+cl_run = jdi.inputs(calculation="browndwarf", climate = True) # start a calculation 
+
+#note you need to put the climate keyword to be True in order to do so
+# now you need to add these parameters to your calculation  
+
+
+teff= 1000 # Effective Temperature of your Brown Dwarf in K
+grav = 1000 # Gravity of your brown dwarf in m/s/s
+
+cl_run.gravity(gravity=grav, gravity_unit=u.Unit('m/(s**2)')) # input gravity
+cl_run.effective_temp(teff) # input effective temperature
+
+# Notice The keyword ck is set to True because you want to use the correlated-k opacities for your calculation
+# and not the line by line opacities
+opacity_ck = jdi.opannection(ck_db=ck_db) # grab your opacities
+
+nlevel = 91 # number of plane-parallel levels in your code
+
+#Lets set the max and min at 1e-4 bars and 500 bars
+
+Pmin = 1e-4 #bars
+Pmax = 500 #bars
+pressure=np.logspace(np.log10(Pmin),np.log10(Pmax),nlevel) # set your pressure grid
+
+temp_guess = np.zeros(shape=(nlevel)) + 500 # K , isothermal atmosphere guess
+
+nofczns = 1 # number of convective zones initially. Let's not play with this for now.
+
+nstr_upper = 83 # top most level of guessed convective zone
+nstr_deep = nlevel -2 # this is always the case. Dont change this
+nstr = np.array([0,nstr_upper,nstr_deep,0,0,0]) # initial guess of convective zones
+
+# Here are some other parameters needed for the code.
+rfacv = 0.0 #we are focused on a brown dwarf so let's keep this as is
+
+cl_run.inputs_climate(temp_guess= temp_guess, pressure= pressure, 
+                      nstr = nstr, nofczns = nofczns , rfacv = rfacv)
+
+out = cl_run.climate(opacity_ck, save_all_profiles=True,with_spec=True)"""
