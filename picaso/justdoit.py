586ffc8ae1681ab2a9e608022e062028045f9ff8
Isaac Malsky <isaacmalsky@gmail.com>
2025-05-15 13:25:55 -0700

    I think I may have accidentally introduced a bug by pushing a slightly old justdoit.py, without the recent integration of fluxes change

--- picaso/justdoit.py
+++ picaso/justdoit.py
@@ -313,8 +313,10 @@ def picaso(bundle,opacityclass, dimension = '1d',calculation='reflected',
             flux_at_top = 0 
 
             if get_lvl_flux: 
+                calc_type=1
                 atm.lvl_output_thermal = dict(flux_minus=0, flux_plus=0, flux_minus_mdpt=0, flux_plus_mdpt=0)
-
+            else: 
+                calc_type=0
 
             for ig in range(ngauss): # correlated-k - loop (which is different from gauss-tchevychev angle)
                 
@@ -330,7 +332,7 @@ def picaso(bundle,opacityclass, dimension = '1d',calculation='reflected',
                                                         atm.level['pressure'],ubar1,
                                                         atm.surf_reflect, atm.hard_surface,
                                                         #setting wno to zero since only used for climate, calctype only gets TOA flx 
-                                                        wno*0, calc_type=0)
+                                                        wno*0, calc_type=calc_type)
                     
                     flux_minus_all_i, flux_plus_all_i, flux_minus_midpt_all_i, flux_plus_midpt_all_i = lvl_fluxes
                     
@@ -340,7 +342,7 @@ def picaso(bundle,opacityclass, dimension = '1d',calculation='reflected',
                                                     DTAU_OG_clear[:,:,ig], W0_no_raman_clear[:,:,ig], COSB_OG_clear[:,:,ig], 
                                                     atm.level['pressure'],ubar1,
                                                     atm.surf_reflect, atm.hard_surface,
-                                                    wno*0, calc_type=0)
+                                                    wno*0, calc_type=calc_type)
                         
                         flux_minus_all_i_clear, flux_plus_all_i_clear, flux_minus_midpt_all_i_clear, flux_plus_midpt_all_i_clear= out_therm_fluxes_clear
                         
@@ -361,11 +363,13 @@ def picaso(bundle,opacityclass, dimension = '1d',calculation='reflected',
                                                 atm.surf_reflect, stream, atm.hard_surface)
 
                 if ((rt_method == 'toon') & get_lvl_flux): 
+                    #ck-table gauss points 
                     atm.lvl_output_thermal['flux_minus']+=flux_minus_all_i*gauss_wts[ig]
                     atm.lvl_output_thermal['flux_plus']+=flux_plus_all_i*gauss_wts[ig]
                     atm.lvl_output_thermal['flux_minus_mdpt']+=flux_minus_midpt_all_i*gauss_wts[ig]
                     atm.lvl_output_thermal['flux_plus_mdpt']+=flux_plus_midpt_all_i*gauss_wts[ig]
-
+                
+                #ck-table gauss points 
                 flux_at_top += flux*gauss_wts[ig]
                 
                 
@@ -563,7 +567,10 @@ def picaso(bundle,opacityclass, dimension = '1d',calculation='reflected',
 
         if ((rt_method == 'toon') & get_lvl_flux): 
             for i in atm.lvl_output_thermal.keys():
-                atm.lvl_output_thermal[i] = compress_thermal(nwno,atm.lvl_output_thermal[i], gweight, tweight)   
+                delta_wno = getattr(opacityclass,'delta_wno', np.concatenate((np.diff(opacityclass.wno),[np.diff(opacityclass.wno)[-1]])))
+                disk_integrated_lvl_flux = compress_thermal(nwno,atm.lvl_output_thermal[i], gweight, tweight)  
+                energy_per_wave_bin = disk_integrated_lvl_flux*delta_wno 
+                atm.lvl_output_thermal[i] = energy_per_wave_bin
 
         #only need to return relative flux if not a browndwarf calculation
         if radius_star == 'nostar': 
@@ -2143,7 +2150,7 @@ class inputs():
         #get a chemistry table from opa if the user supplied it and it exists
         chemistry_table = getattr(opa, 'full_abunds', None)
 
-        #now chemistry options can be enforced 
+        #now chemistry options can be enforced basically doing chemical equilibrium / photochem / only chemeq included
         self.chemistry_handler(chemistry_table=chemistry_table)
 
         #if a quench level dictionary is provided 
