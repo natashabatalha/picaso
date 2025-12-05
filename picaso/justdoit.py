59b209d1f93f68e746dd97927781ff8b1179b571
natashabatalha <natasha.e.batalha@gmail.com>
2025-12-05 10:47:30 -0800

    introduces hardware global variable to justdoit.py

--- picaso/justdoit.py
+++ picaso/justdoit.py
@@ -43,7 +43,10 @@ import h5py
 # #testing error tracker
 # from loguru import logger 
 __refdata__ = os.environ.get('picaso_refdata')
-__version__ = '4.0'
+__version__ = '5.0'#GPU VERSION
+
+#sets the hardware (Default is CPU)
+__hardware__ = os.environ.get('picaso_hardware','cpu')
 
 
 if not os.path.exists(__refdata__): 
